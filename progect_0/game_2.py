630ce5f0d2d89df9edcf2feeb8615e498f92528d
SergMust <123100826+SergMust@users.noreply.github.com>
2023-01-21 18:57:28 +0500

    edit advanced algorithm

--- progect_0/game_2.py
+++ progect_0/game_2.py
@@ -49,7 +49,6 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-
-#if __name__=='__main__':
-# RUN
-score_game(random_predict)
+#RUN
+if __name__=='__main__':
+    score_game(random_predict)
